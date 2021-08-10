9b26e973165451583d2fbc924476f50371a18374
ZIR3A <saranbrl35@gmail.com>
2021-08-10 13:14:17 +0545

    last commit

--- includes/db.php
+++ includes/db.php
@@ -1,4 +1,4 @@
 <?php
 // $con=mysqli_connect("localhost","root","","plant_shop");
-$con = mysqli_connect("remotemysql.com", "69v3kWP3fl", "VQ88LrJeAr", "69v3kWP3fl");
+$con = mysqli_connect("remotemysql.com", "69v3kWP3fl", "WDbg8vaGII", "69v3kWP3fl");
 ?>
